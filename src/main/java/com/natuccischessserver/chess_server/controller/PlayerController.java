0f39f8652e723c1d5672ba88be10f140c7e4ea46
GabrielNatucci <gabriel.natuccirusso@hotmail.com>
2024-03-04 12:26:12 -0300

    cross origin

--- src/main/java/com/natuccischessserver/chess_server/controller/PlayerController.java
+++ src/main/java/com/natuccischessserver/chess_server/controller/PlayerController.java
@@ -1,6 +1,7 @@
 package com.natuccischessserver.chess_server.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import com.natuccischessserver.chess_server.service.PlayerService;
 
 @RestController
 @RequestMapping("/player")
+@CrossOrigin
 public class PlayerController {
     @Autowired
     private PlayerService playerService;
@@ -25,7 +27,7 @@ public class PlayerController {
         }
     }
 
-    @GetMapping("/get")
+    @GetMapping("/login")
     public String logInPlayer(@RequestBody Player p) {
         if (playerService.logInPlayer(p.getEmail(), p.getPassword()) == null) {
             return "Não achei você";
