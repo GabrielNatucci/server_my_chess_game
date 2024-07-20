50d2cd5ca7a3b7187a63ca88154fe9e4143ae8c5
GabrielNatucci <gabriel.natuccirusso@hotmail.com>
2024-07-20 10:53:26 -0300

    Algumas adaptações para receber coisas novas do front
    envolvendo as propriedades da classe Move

--- src/main/java/com/natuccischessserver/chess_server/websockets/Move.java
+++ src/main/java/com/natuccischessserver/chess_server/websockets/Move.java
@@ -1,37 +1,66 @@
 package com.natuccischessserver.chess_server.websockets;
 
+import java.util.ArrayList;
+
 import com.natuccischessserver.chess_server.model.Player;
 
 public class Move {
     private Player player;
-	private String move_str;
-	private String color;
+    private ArrayList<Integer> start_square;
+    private ArrayList<Integer> end_square;
+    private int status;
+    private String color;
 
-	public Player getPlayer() {
-		return player;
-	}
-
-	public void setPlayer(Player player) {
-		this.player = player;
-	}
-
-	public String getMove_str() {
-		return move_str;
-	}
-	public void setMove_str(String move_str) {
-		this.move_str = move_str;
-	}
-
-	public String getColor() {
-		return color;
-	}
-	public void setColor(String color) {
-		this.color = color;
-	}
-
-    public Move(Player player, String move_str, String color) {
+    public Move(Player player, ArrayList<Integer> start_square, ArrayList<Integer> end_square, int status,
+            String color) {
         this.player = player;
-        this.move_str = move_str;
+        this.start_square = start_square;
+        this.end_square = end_square;
+        this.status = status;
+        this.color = color;
+    }
+
+    public Move(int status) {
+        this.status = status;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public ArrayList<Integer> getStart_square() {
+        return start_square;
+    }
+
+    public void setStart_square(ArrayList<Integer> start_square) {
+        this.start_square = start_square;
+    }
+
+    public ArrayList<Integer> getEnd_square() {
+        return end_square;
+    }
+
+    public void setEnd_square(ArrayList<Integer> end_square) {
+        this.end_square = end_square;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
         this.color = color;
     }
 
--- src/main/java/com/natuccischessserver/chess_server/websockets/MoveController.java
+++ src/main/java/com/natuccischessserver/chess_server/websockets/MoveController.java
@@ -1,18 +1,31 @@
 package com.natuccischessserver.chess_server.websockets;
 
+import java.util.ArrayList;
+
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+
+import com.natuccischessserver.chess_server.model.Player;
 
 @Controller
 public class MoveController {
     @MessageMapping("/move")
     @SendTo("/move_resp")
     public Move move(Move move) throws Exception {
-        // remove os " " da string
-        String move_str = move.getMove_str().replaceAll("\\s", "");
+        try {
+            // remove os " " da string
+            ArrayList<Integer> start_square = move.getStart_square();
+            ArrayList<Integer> end_square = move.getEnd_square();
 
-        // retorna o movimento pra quem tiver inscrito no jogo
-        return new Move(move.getPlayer(), move_str, move.getColor());
+            Player player = move.getPlayer();
+
+            player.setAuthtoken("");
+
+            // retorna o movimento pra quem tiver inscrito no jogo
+            return new Move(player, start_square, end_square, 200, move.getColor());
+        } catch (Exception e) {
+            return new Move(200);
+        }
     }
 }
